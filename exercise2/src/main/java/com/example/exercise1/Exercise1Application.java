475bddbc369057c8a2ccf54e59f7699b815482dd
Mamadoukernel <kernel0748@gmail.com>
2023-01-25 09:54:17 +0000

    Exercice_Xp DU DAY3 WEEK5

---
+++ exercise2/src/main/java/com/example/exercise1/Exercise1Application.java
@@ -0,0 +1,13 @@
+package com.example.exercise1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise1Application.class, args);
+	}
+
+}
